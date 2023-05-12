b411700cebae3ccee07c76dd1206053b7c50209d
enjoy <enjoy_azad@sina.com>
2023-05-12 20:41:22 +0800

    test

---
+++ mod_thread/src/main/java/com/sum/mod_thread/ThreadCpuActivity.kt
@@ -0,0 +1,11 @@
+package com.sum.mod_thread
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class ThreadCpuActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_thread_cpu)
+    }
+}
